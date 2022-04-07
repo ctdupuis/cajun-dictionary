99bbb026ea7a3cf0fb65d5072a15829e4f615c04
Cody Dupuis <54044737+ctdupuis@users.noreply.github.com>
2022-04-07 13:48:10 -0500

    terms index page correctly renders terms

--- next-cajun-dictionary/pages/terms/index.jsx
+++ next-cajun-dictionary/pages/terms/index.jsx
@@ -1,12 +1,35 @@
 import axios from 'axios';
 
 export default function TermList(props) {
+    const letters = props.terms.map(term => term.name.slice(0, 1).toUpperCase())
+
+    const uniqLetters = letters.filter((letter, index, self) => self.indexOf(letter) == index)
+    
+    const titles = uniqLetters.map(letter => {
+        let termsOfThisLetter = props.terms.filter(term => term.name.charAt(0).toUpperCase() === letter).map(term => {
+            return(
+                <div className="flex space-bet">
+                    <a className="list">{term.name}</a>
+                    <span>{term.username}</span>
+                </div>
+            )
+        })
+  
+        return(
+            <div className="word-wrapper">
+                <div className="list-title flex space-bet">
+                    <h3>{letter}</h3><h3>Submitted by</h3>
+                </div>
+                {termsOfThisLetter}
+            </div>
+        )
+    })
     
     return (
         <div className="wrapper">
             <section className="container bg-red1">
                 <div className='container bg-white'>
-                    
+                    {titles}
                 </div>
             </section>
         </div>
