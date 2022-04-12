6cd0b2465a780749b1cdf74f9feff30bbe6787e2
Cody Dupuis <54044737+ctdupuis@users.noreply.github.com>
2022-04-12 15:18:03 -0500

    goofy context example for myself later on

--- next-cajun-dictionary/pages/index.js
+++ next-cajun-dictionary/pages/index.js
@@ -1,8 +1,18 @@
 import TermCard from '../components/ui/Terms/TermCard';
+import { useAppContext } from '../store/app';
 
 export default function Home() {
+  const { user: { currentUser, setCurrentUser } } = useAppContext();
+
+  const login = () => {
+    currentUser ? setCurrentUser(undefined) : setCurrentUser({ name: 'Cody' })
+  }
+
   return (
     <div className="wrapper">
+
+      { currentUser ? <h1>{currentUser.name}</h1> : <h1>No user in context</h1>}
+      <button onClick={login}>Login</button>
       <div className="flex space-even">
         <TermCard type={'term-of-day'} />
         <TermCard type={'most-liked'} />
