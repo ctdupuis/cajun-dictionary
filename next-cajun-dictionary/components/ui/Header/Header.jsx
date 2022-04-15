ba524f46baf8a7eb49975dba10f0ccf5f535835d
Cody Dupuis <54044737+ctdupuis@users.noreply.github.com>
2022-04-15 12:00:09 -0500

    reworked the header

--- next-cajun-dictionary/components/ui/Header/Header.jsx
+++ next-cajun-dictionary/components/ui/Header/Header.jsx
@@ -1,14 +1,31 @@
 import classes from './header.module.css';
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className="bg-gradient">
-        <div className="container flex space-bet center-align">
-            <h1 className={classes.logo}>Cajun Dictionary</h1>
+        <div className={classes.main_head}>
+          <div className={classes.logo_container}>
+            <h2 className={classes.logo}>
+              <Link href='/'>
+                Cajun Dictionary
+              </Link>
+            </h2>
 
-            <div className={classes.session_nav}>
-                <a href="/auth">Login</a>
+            <div className={classes.nav_item}>
+                <a href="/terms">All Terms</a>
             </div>
+            <div className={classes.nav_item}>
+                <a href="/about">About</a>
+            </div>
+            <div className={classes.nav_item}>
+                <a href="/terms/new">New</a>
+            </div>
+          </div>
+
+          <div className={classes.session_nav}>
+              <a href="/auth">Login</a>
+          </div>
         </div>
     </header>
   )
