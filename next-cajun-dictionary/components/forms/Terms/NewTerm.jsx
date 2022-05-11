6179c2f81c53fb9b087a2d2b753c82b18501e4aa
Cody Dupuis <54044737+ctdupuis@users.noreply.github.com>
2022-05-11 09:39:51 -0500

    fleshed out new term from

--- next-cajun-dictionary/components/forms/Terms/NewTerm.jsx
+++ next-cajun-dictionary/components/forms/Terms/NewTerm.jsx
@@ -16,34 +16,34 @@ export default function NewTermForm() {
     <form className={classes.form} onSubmit={e => e.preventDefault()}>
       <label htmlFor='name'>Your Term</label>
       <input 
-      className={classes.input} 
-      type='text' 
-      placeholder='The name of your term here'
-      value={name} 
-      onChange={e => setName(e.target.value)} />
+        className={classes.input} 
+        type='text' 
+        placeholder='The name of your term here'
+        value={name} 
+        onChange={e => setName(e.target.value)} />
 
       <label htmlFor='pronounce'>Pronunciation</label>
       <input 
-      className={classes.input} 
-      type='text' 
-      placeholder='Try to guide others on how to pronounce your term'
-      value={pronounce} 
-      onChange={e => setPronounce(e.target.value)} />
+        className={classes.input} 
+        type='text' 
+        placeholder='Try to guide others on how to pronounce your term'
+        value={pronounce} 
+        onChange={e => setPronounce(e.target.value)} />
 
       <label htmlFor='definition'>Definition</label>
       <textarea 
-      className={classes.input} 
-      type='text'
-      placeholder='What does your term mean?' 
-      value={definition} 
-      onChange={e => setDefinition(e.target.value)} />
+        className={classes.input} 
+        type='text'
+        placeholder='What does your term mean?' 
+        value={definition} 
+        onChange={e => setDefinition(e.target.value)} />
 
       <label htmlFor='name'>Use Case Example</label>
       <textarea 
-      placeholder='Type an example of how to use this term in a sentence. Please wrap your sentence in double quotes ("")' 
-      className={classes.input}
-      value={useCase}
-      onChange={e => setUseCase(e.target.value)}></textarea>
+        placeholder='Type an example of how to use this term in a sentence. Please wrap your sentence in double quotes ("")' 
+        className={classes.input}
+        value={useCase}
+        onChange={e => setUseCase(e.target.value)}></textarea>
       
       <Button text={'submit'} handleClick={submitTerm} />
     </form>
